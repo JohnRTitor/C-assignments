22ce49b56a25eef2c06456a3924af765b5d0ed53
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-08-09 23:18:42 +0530

    Complete quick sort

--- Extras/Array - Quick Sort Iterative.c
+++ Extras/Array - Quick Sort Iterative.c
@@ -15,17 +15,22 @@
 
 #define MAX_SIZE 100
 
-struct tuple
-{
-    int left;
-    int right;
-};
-
 int main ()
 {
+    int arr[MAX_SIZE];
+    int size;
+
+    size = readArray(arr);
+    printf("The array before sorting: ");
+    printArray(arr, size);
+    quickSortIterative(arr, size);
+    printf("The array after sorting: ");
+    printArray(arr, size);
     return 0;
 }
-
+/// @brief reads an array and its size from the user
+/// @param arr is the array to be read
+/// @return the size of the array
 int readArray (int arr[])
 {
     int size;
@@ -33,7 +38,7 @@ int readArray (int arr[])
     scanf("%d", &size);
     if (size < 1 ||size > MAX_SIZE)
     {
-        fprintf(stderr, "Error: Size out of bounds.");
+        fprintf(stderr, "Error: Size out of bounds.\n");
         exit(EXIT_FAILURE);
     }
     printf("Enter the elements of the array: ");
@@ -44,6 +49,9 @@ int readArray (int arr[])
     return size;
 }
 
+/// @brief prints the array seperated by a space
+/// @param arr is the array to be printed
+/// @param size size of the array
 void printArray (int arr[], int size)
 {
     for (int i = 0; i < size; i++)
@@ -53,6 +61,9 @@ void printArray (int arr[], int size)
     printf("\n");
 }
 
+/// @brief swaps two integers
+/// @param a is the first integer, passed by reference
+/// @param b is the second integer, passed by reference
 void swap (int *a, int *b)
 {
     int temp = *a;
@@ -86,4 +97,43 @@ int partition (int arr[], int lower, int upper)
     }
     swap(&arr[lower], &arr[j]);
     return j;
+}
+
+/// @brief sorts the array using quick sort
+/// @param arr is the array to be sorted
+/// @param size is the size of the array
+void quickSortIterative (int arr[], int size)
+{
+    int lower, upper;
+    int stack[MAX_SIZE * 2];
+    int top = -1;
+    int pivotIndex;
+
+    // push the initial values of lower and upper into the stack
+    stack[++top] = lower = 0;
+    stack[++top] = upper = size - 1;
+
+    while (top != -1) // until stack becomes empty
+    {
+        // pop upper and lower
+        upper = stack[top--];
+        lower = stack[top--];
+
+        // set pivot element at its correct position in sorted array and partition from pivot
+        pivotIndex = partition(arr, lower, upper);
+
+        if (pivotIndex - 1 > lower) // if there are elements on the left side of pivot
+        {
+            // push the lower and upper bounds of the left subarray into the stack
+            stack[++top] = lower;
+            stack[++top] = pivotIndex - 1;
+        }
+
+        if (pivotIndex + 1 < upper) // if there are elements on the right side of pivot
+        {
+            // push the lower and upper bounds of the right subarray into the stack
+            stack[++top] = pivotIndex + 1;
+            stack[++top] = upper;
+        }
+    }
 }
