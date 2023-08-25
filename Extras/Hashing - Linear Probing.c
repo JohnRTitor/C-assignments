e50e044a434c1cbfd9114916d8c6b55b760ab57c
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-08-25 13:39:22 +0000

    Hashing added

--- Extras/Hashing - Linear Probing.c
+++ Extras/Hashing - Linear Probing.c
@@ -1,32 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #define MAX_SIZE 100
 #define EMPTY -1
 
-int hash (int key, int size)
+void init (int arr[]);
+
+int hash (int key)
 {
-    return key % size;
+    return key % MAX_SIZE;
 }
 
-int linearProbing (int arr[], int key, int size)
+int linearProbing (int arr[], int key)
 {
-    while ()
-        if (arr[hash(key, size)] == EMPTY)
-            return hash()
+    int index = hash(key);
+    int attempts = 0;
+    while (true)
+    {
+        if (arr[index] == EMPTY || arr[index] == key)
+            return index;
+        else
+        {
+            index = (hash(key) + attempts) % MAX_SIZE;
+            attempts++;
+            if (attempts == MAX_SIZE)
+                return -1;
+        }
+    }
 }
 
 int main ()
 {
     int size, arr[MAX_SIZE];
+    int data, index;
 
     init(arr);
-    printf("Enter the size:");
+    printf("Enter the size: ");
     scanf("%d", &size);
 
-    printf("Enter the elements:");
+    printf("Enter the elements: ");
     for (int i = 0; i < size; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d", &data);
+        int index = linearProbing(arr, data);
+        if (index == -1)
+            fprintf(stderr, "Hash table is full\n");
+        else
+            arr[index] = data;
     }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &data);
+    index = linearProbing(arr, data);
+    if (index == -1)
+        printf("Element not found\n");
+    else
+        printf("Element found at index %d\n", index);
 }
 
 void init (int arr[])
